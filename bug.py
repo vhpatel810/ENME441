f513779d528d6517c192b114ed5d1cf7be295737
vhpatel810 <vhpatel810@gmail.com>
2025-10-20 01:15:59 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -14,19 +14,20 @@ GPIO.setup(S3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 try:
 	shift = Shifter(23,25,24)
 	timedefault = 0.10
-	bug = Bug(shift, timestep = timedefault, x = 4, isWrapON= False)
+	bug = Bug(shift, timestep = timedefault, x = 4, isWrapOn=False)
 	laststate = GPIO.input(S2)
 	while True: 
 		if GPIO.input(S1) == GPIO.LOW:
 			bug.start()
 		else: 
 			bug.stop()
-	current_s2 = GPIO.input(S2)
-	if laststate == GPIO.HIGH and current_s2 ==GPIO.LOW:
-		bug.isWrapOn = not bug.isWrapON
-	laststate = current_s2
+		current_s2 = GPIO.input(S2)
+		if laststate == GPIO.HIGH and current_s2 ==GPIO.LOW:
+			bug.isWrapOn = not bug.isWrapOn
+		laststate = current_s2
 
-	bug.timestep= (timedefault/3) if (GPIO.input (S3)== GPIO.LOW) else timedefault
-	bug.repeat()
+		bug.timestep= (timedefault/3) if (GPIO.input (S3)== GPIO.LOW)else timedefault
+		bug.repeat()
 except KeyboardInterrupt:
 	GPIO.cleanup()
+
